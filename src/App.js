02bac3033bbfe314d7c5df593d3dad85c6c6c893
rndev2-broadmoor <87381525+rndev2-broadmoor@users.noreply.github.com>
2022-06-16 19:46:39 +0500

    login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import Login from './Components/Login/Login';
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index path='/' element={<Home />} />
+        <Route index path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import "./Login.css";
+
+const Login = () => {
+  const [option, setOption] = useState(1);
+  return <>
+    <div className='login-fields-parent'>
+      <section>
+        <h1 className='Login-heading'>Dashboard Login</h1>
+        <section className='option-parent'>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="radio-buttons-group"
+          >
+            <section style={{ display: 'flex', justifyContent: 'center', width: '350px' }}>
+              <FormControlLabel onChange={() => setOption(1)} value="female" control={<Radio />} label="Store Owner" />
+              <FormControlLabel onChange={() => setOption(2)} value="male" control={<Radio />} label="Store Manager" />
+            </section>
+          </RadioGroup>
+        </section>
+        <section style={{ marginTop: '1rem' }}>
+          {
+            option === 2 &&
+            <>
+              <TextField style={{ width: '350px' }} size='small' id="outlined-basic" label="Store ID" variant="outlined" /><br /><br />
+              <TextField style={{ width: '350px' }} size='small' id="outlined-basic" label="Username" variant="outlined" /><br /><br />
+              <TextField style={{ width: '350px' }} size='small' id="outlined-basic" label="Password" variant="outlined" />
+            </>
+          }
+          {
+            option === 1 &&
+            <>
+              <TextField style={{ width: '350px' }} size='small' id="outlined-basic" label="Email" variant="outlined" /><br /><br />
+              <TextField style={{ width: '350px' }} size='small' id="outlined-basic" label="Password" variant="outlined" />
+            </>
+          }
+        </section>
+        <section className='action-parent'>
+          <Button variant="contained">Login</Button>
+          <span style={{ textAlign: 'center', marginTop: '10px', cursor: 'pointer' }}>Forget Password</span>
+        </section>
+      </section>
+    </div>
+  </>
+}
+
+export default Login;
