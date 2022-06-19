ff2620c4e06dc25337d4e930073791d5231f9667
rndev2-broadmoor <87381525+rndev2-broadmoor@users.noreply.github.com>
2022-06-19 16:42:43 +0500

    add table in users

--- src/Components/Dashboard/Client/UserManagement/Management/Management.js
+++ src/Components/Dashboard/Client/UserManagement/Management/Management.js
@@ -1,32 +1,27 @@
 import React, { useState } from 'react';
 import Drawer from '../../../../Layout/Drawer/Drawer';
 import './Management.css';
-import Card from './Card';
 import AddIcon from '../../Store/imgs/add.png';
 import AddUser from '../AddUser/AddUser';
+import Users from './Users';
 
 const Management = () => {
-    const array = [1, 2, 3, 4, 5, 6];
     const [action, setAction] = useState({ open: false, type: null });
 
     return <div className='menu-parent'>
         <section className='drawer-parent'>
             <Drawer />
         </section>
-        <section className='dash-content-parent'>
+        <section className='dash-content-parent user-manage-parent'>
             <section className='page-heading'>
-                <span className='menu-text'>User Management</span>
+                {/* <span className='menu-text'>User Management</span> */}
+                <section className='add-store-action-parent add-store-action'>
+                    <img style={{ width: '40px', position: 'relative', right: '14px' }} src={AddIcon} alt='add' />
+                    <span onClick={() => setAction({ open: true, type: 'Add' })} className='add-store-text'>Add User</span>
+                </section>
             </section>
-            <section className='grid-parent-cards' style={{ marginTop: '1rem' }}>
-                {
-                    array?.map((item, index) => {
-                        return <Card setAction={setAction} key={index} />
-                    })
-                }
-            </section>
-            <section className='add-store-action-parent'>
-                <img style={{ width: '40px' }} src={AddIcon} alt='add' />
-                <span onClick={() => setAction({ open: true, type: 'Add' })} className='add-store-text'>Add User</span>
+            <section className='' style={{ marginTop: '1rem' }}>
+                <Users setAction={setAction} />
             </section>
         </section>
         <AddUser action={action} setAction={setAction} />
